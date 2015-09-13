2c2347aa4097b4273ebc341fbbc9d95e0833ca01
origooo <gusbacpa@student.gu.se>
2015-09-13 17:36:54 +0200

    Templates fiddle added

--- template.cpp
+++ template.cpp
@@ -2,20 +2,22 @@
 
 using namespace std;
 
-template <class T, int multiplier> void multiply(T &val) {
-	val *= multiplier;
+template <typename T, typename M> void multiply(T val, M multiplier) {
+	*val *= *multiplier;
 }
 
 int main() {
-	int a = 2;
-	double b = 2.12345;
+	double m = 2, *m_ptr = &m;;
+	
+	int a = 2, *a_ptr = &a;
+	double b = 2.12345, *b_ptr = &b;
 
 	std::cout << a << endl;
-	multiply<decltype(a),2>(a);
+	multiply<decltype(a_ptr),decltype(m_ptr)>(a_ptr,m_ptr);
 	std::cout << a << endl;
 
 	std::cout << b << endl;
-	multiply<decltype(b),2>(b);
+	multiply<decltype(b_ptr),decltype(m_ptr)>(b_ptr,m_ptr);
 	std::cout << b << endl;
 
 	return 0;
